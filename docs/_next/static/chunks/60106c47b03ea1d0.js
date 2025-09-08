02b8880ed326035c87ef23d8a7a70fef165940d1
gastibarroule <gastonibarroule@gmail.com>
2025-09-08 16:56:55 +0200

    chore(docs): export latest Next.js build to docs

---
+++ docs/_next/static/chunks/60106c47b03ea1d0.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/b0f7527c59306e97.js",
+  "static/chunks/767d75d60816c217.js",
+  "static/chunks/turbopack-93a9b6b44746d321.js"
+])
---
+++ docs/_next/static/chunks/83d4eaba1443000c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/c7c215f287a7a36f.js",
+  "static/chunks/767d75d60816c217.js",
+  "static/chunks/turbopack-3656fae93207174b.js"
+])
